4576235447ed3a764a2fab1e439cb6b5744c2f70
togo-mentor <uint2435@gmail.com>
2021-08-08 14:48:30 +0900

    cors設定変更

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -12,6 +12,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      expose: ['Access-Control-Allow-Origin']
+      expose: ['Access-Control-Allow-Origin'],
+      credentials: true
   end
 end
